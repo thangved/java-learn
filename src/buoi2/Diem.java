0da16b550676451cb8a69a54c9060b5b8e10f36f
thangved <thangcreation@gmail.com>
2021-09-22 14:24:53 +0700

    fix

--- src/buoi2/Diem.java
+++ src/buoi2/Diem.java
@@ -3,12 +3,7 @@ package buoi2;
 import java.util.Scanner;
 
 public class Diem {
-<<<<<<< HEAD
-    private int x,
-            y;
-=======
     private int x, y;
->>>>>>> 0b21f16114808059e160fcf6f27b12546ee4d18c
 
     public Diem() {
         x = 0;
@@ -20,32 +15,11 @@ public class Diem {
         this.y = y;
     }
 
-<<<<<<< HEAD
-    public Diem(Diem d){
+    public Diem(Diem d) {
         x = d.x;
         y = d.y;
     }
 
-    public void nhap() {
-        Scanner scanner = new Scanner(System.in);
-        int x, y;
-        x = scanner.nextInt();
-        y = scanner.nextInt();
-        this.x = x;
-        this.y = y;
-        scanner.close();
-    }
-
-    public void in() {
-        System.out.format("(%d, %d)\n", x, y);
-    }
-
-    public void doiDiem(int dx, int dy) {
-        x += dx;
-        y += dy;
-    }
-
-=======
     public void nhap() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhap diem x:");
@@ -59,7 +33,11 @@ public class Diem {
         System.out.format("(%d, %d)\n", x, y);
     }
 
->>>>>>> 0b21f16114808059e160fcf6f27b12546ee4d18c
+    public void doiDiem(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
     public int giaTriX() {
         return x;
     }
@@ -68,25 +46,11 @@ public class Diem {
         return y;
     }
 
-<<<<<<< HEAD
     public float khoangCach(Diem d) {
         return (float) Math.sqrt(Math.pow(x - d.x, 2) + Math.pow(y - d.y, 2));
     }
 
     public float khoangCach() {
         return khoangCach(new Diem());
-=======
-    public void doiDiem(int dx, int dy) {
-        x += dx;
-        y += dy;
-    }
-
-    public float khoangCach() {
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    public float khoangCach(Diem d) {
-        return (float) Math.sqrt(Math.pow(x - d.x, 2) + Math.pow(y - d.y, 2));
->>>>>>> 0b21f16114808059e160fcf6f27b12546ee4d18c
     }
 }
